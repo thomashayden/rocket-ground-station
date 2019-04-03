4de5b5b0abd0c8f6b633e6b583f98245d379cfe5
Thomas Hayden <hayden.t@husky.neu.edu>
2019-04-02 22:15:39 -0400

    Modify gpio watcher

--- check_gpio.sh
+++ check_gpio.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 echo "4" > /sys/class/gpio/export
 echo "in" > /sys/class/gpio/gpio4/direction
+echo "3" > /sys/class/gpio/export
+echo "in" > /sys/class/gpio/gpio3/direction
+
 
 if [[ $(cat /sys/class/gpio/gpio4/value) == 1 ]]
 then
     sleep 0.1
     if [[ $(cat /sys/class/gpio/gpio4/value) == 1 ]]
     then
-        rm ./comm/*
-        touch ./comm/gpio5high
+        rm ./comm/gpio4high
+        touch ./comm/gpio4low # These are the wrong way around because it is normally high while open
     fi
 else
     sleep 0.1
     if [[ $(cat /sys/class/gpio/gpio4/value) != 1 ]]
     then
-        rm ./comm/*
-        touch ./comm/gpio4low
+        rm ./comm/gpio4low
+        touch ./comm/gpio4high
+    fi
+fi
+
+
+if [[ $(cat /sys/class/gpio/gpio3/value) == 1 ]]
+then
+    sleep 0.1
+    if [[ $(cat /sys/class/gpio/gpio3/value) == 1 ]]
+    then
+        rm ./comm/gpio3high
+        touch ./comm/gpio3low # These are the wrong way around because it is normally high while open
+    fi
+else
+    sleep 0.1
+    if [[ $(cat /sys/class/gpio/gpio3/value) != 1 ]]
+    then
+        rm ./comm/gpio3low
+        touch ./comm/gpio3high
     fi
 fi
 
 echo "4" >> /sys/class/gpio/unexport
+echo "3" >> /sys/class/gpio/unexport
